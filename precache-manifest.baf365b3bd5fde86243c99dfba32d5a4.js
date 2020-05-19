819b31d231989512ffd5e22fdf35bd0a580a9be7
jerin1498 <jerinhappy1498@gmail.com>
2020-05-19 22:20:01 +0530

    Updates

---
+++ precache-manifest.baf365b3bd5fde86243c99dfba32d5a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb7fa92bd17609c3476a5900e806f8a",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "aa4643d7938876cc4c9f",
+    "url": "/monster/static/css/main.4bf9fbcd.chunk.css"
+  },
+  {
+    "revision": "83708b8d3de79eae6b22",
+    "url": "/monster/static/js/2.833f470a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.833f470a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa4643d7938876cc4c9f",
+    "url": "/monster/static/js/main.8b1bfa8c.chunk.js"
+  },
+  {
+    "revision": "627926c7c91a8f7b6f29",
+    "url": "/monster/static/js/runtime-main.d18ebdc4.js"
+  }
+]);
